0d319325c35f407a30c1930cda70d2651c999048
dkotschessa <itzdavey@gmail.com>
2022-04-04 15:03:13 -0400

    bookmarking for backup

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,9 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
+
+
 class HomePageTestCase(TestCase):
 
 
@@ -36,16 +38,25 @@ class NewListTest(TestCase):
 
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        first_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -54,9 +65,16 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
+
+    def test_displays_all_items(self):
+        list_= List.objects.create()
+        Item.objects.create(text = 'itemy1', list=list_)
+        Item.objects.create(text = 'itemy2', list = list_)
 
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
